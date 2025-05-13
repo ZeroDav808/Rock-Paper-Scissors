47e461391a613bde8cd0b2e9ed3580ca853edf56
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-05-13 18:39:27 -0500

    Added function to check if winning condition is met. If so, output message is printed and scores reset

--- index.js
+++ index.js
@@ -2,38 +2,52 @@ const choices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 
+const textBox = document.querySelector("p");
+const buttons = document.querySelectorAll("button");
+
+
 function playRound(hChoice, cChoice) {
+  textBox.textContent = "";
   if (hChoice === choices[0]) {
     if (cChoice === choices[0]) {
-        console.log(`It's a tie!`);     
+      textBox.textContent = `It's a tie!`;
     } else if (cChoice === choices[1]) {
       computerScore++;
-      console.log(`Computer beats human! ${printScore()}`);
+      textBox.textContent = `Computer beats human! ${printScore()}`;
     } else {
       humanScore++;
-      console.log(`Human beats computer! ${printScore()}`);
+      textBox.textContent = `Human beats computer! ${printScore()}`;
     }
   } else if (hChoice === choices[1]) {
     if (cChoice === choices[0]) {
-        humanScore++;
-        console.log(`Human beats computer! ${printScore()}`);
+      textBox.textContent = `Human beats computer! ${printScore()}`;
     } else if (cChoice === choices[1]) {
-        console.log(`It's a tie!`);     
+      textBox.textContent = `It's a tie!`;
     } else {
-        computerScore++;
-        console.log(`Computer beats human! ${printScore()}`);
+      computerScore++;
+      textBox.textContent = `Computer beats human! ${printScore()}`;
     }
   } else if (hChoice === choices[2]) {
     if (cChoice === choices[0]) {
-        computerScore++;
-        console.log(`Computer beats human! ${printScore()}`);
+      computerScore++;
+      textBox.textContent = `Computer beats human! ${printScore()}`;
     } else if (cChoice === choices[1]) {
-        humanScore++;
-        console.log(`Human beats computer! ${printScore()}`);
+      humanScore++;
+      textBox.textContent = `Human beats computer! ${printScore()}`;
     } else {
-        console.log(`It's a tie!`);     
+      textBox.textContent = `It's a tie!`;
     }
   }
+}
+
+function gameEnd() {
+    if(humanScore === 5){
+        textBox.textContent = `Human wins the game! ${printScore()}`;
+        humanScore = computerScore = 0;
+    } else if(computerScore === 5){
+        textBox.textContent = `Computer wins the game! ${printScore()}`;
+        humanScore = computerScore = 0;
+    }
 }
 
 function getComputerChoice() {
@@ -42,31 +56,31 @@ function getComputerChoice() {
 }
 
 function handleClick(e) {
-    const value = e.target.id;
-    const cChoice = getComputerChoice();
-    switch(value) {
-        case 'one':
-            playRound(choices[0], cChoice);
-            break;
-        case 'two':
-            playRound(choices[1], cChoice);
-            break;
-        case 'three':
-            playRound(choices[2], cChoice);
-            break;
-        default:
-            console.log('???');
-    }
+  const value = e.target.id;
+  const cChoice = getComputerChoice();
+  switch (value) {
+    case "one":
+      playRound(choices[0], cChoice);
+      gameEnd();
+      break;
+    case "two":
+      playRound(choices[1], cChoice);
+      gameEnd();
+      break;
+    case "three":
+      playRound(choices[2], cChoice);
+      gameEnd();
+      break;
+    default:
+      console.log("???");
+  }
 }
 
-const textBox = document.querySelector('p');
-const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
-    button.addEventListener('click', handleClick);
+  button.addEventListener("click", handleClick);
 });
 
 function printScore() {
   return `The score is: User-> ${humanScore}, Computer-> ${computerScore}`;
 }
-
